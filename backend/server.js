297936d77b602eb694e17bf133b4056a78a6223a
minhvnh <vnhminh_work@gmail.com>
2026-02-04 23:39:43 +0700

    fix build failed

--- backend/server.js
+++ backend/server.js
@@ -122,7 +122,7 @@ app.get('/api/health', (req, res) => {
 const clientPath = path.join(__dirname, '../dist');
 app.use(express.static(clientPath));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(clientPath, 'index.html'));
 });
 
